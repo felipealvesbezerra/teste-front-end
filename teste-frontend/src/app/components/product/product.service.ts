16aa90e45fd5de14b63aaedb88db701f77109113
Gabriel Crespo <gabriel.crespo1942@gmail.com>
2020-11-01 19:16:55 -0300

    Adicionando mensagens genéricas de erro

--- teste-frontend/src/app/components/product/product.service.ts
+++ teste-frontend/src/app/components/product/product.service.ts
@@ -1,50 +1,78 @@
+import { catchError, map } from 'rxjs/operators';
 import { Product } from './product.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
   baseUrl = 'http://ec2-52-203-6-72.compute-1.amazonaws.com:8000/products';
 
-  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
 
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'X', {
       duration: 3000,
-      horizontalPosition: "right",
-      verticalPosition: "top"
-    })
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: isError ? ['msg-fail'] : ['msg-success'],
+    });
   }
-
-  read(): Observable<Product[]>{
-    const url = `${this.baseUrl}/list`;
-    return this.http.get<Product[]>(url);
-  }
-
 
   create(product: Product): Observable<Product> {
     const url = `${this.baseUrl}/create`;
-    return this.http.post<Product>(url, product, {responseType: 'text' as 'json'});
+    return this.http
+      .post<Product>(url, product, { responseType: 'text' as 'json' })
+      .pipe(
+        map((object) => object),
+        catchError((error) => this.handleError(error))
+      );
+  }
+
+  read(): Observable<Product[]> {
+    const url = `${this.baseUrl}/list`;
+    return this.http.get<Product[]>(url).pipe(
+      map((object) => object),
+      catchError((error) => this.handleError(error))
+    );
   }
 
   readById(id: string): Observable<Product> {
     const url = `${this.baseUrl}/${id}`;
-    return this.http.get<Product>(url);
+    return this.http.get<Product>(url).pipe(
+      map((object) => object),
+      catchError((error) => this.handleError(error))
+    );
   }
 
   update(product: Product): Observable<Product> {
     const url = `${this.baseUrl}/${product._id}/update`;
-    return this.http.put<Product>(url, product, {responseType: 'text' as 'json'});
+    return this.http
+      .put<Product>(url, product, {
+        responseType: 'text' as 'json',
+      })
+      .pipe(
+        map((object) => object),
+        catchError((error) => this.handleError(error))
+      );
   }
 
-  delete(id: string): Observable<Product>{
+  delete(id: string): Observable<Product> {
     const url = `${this.baseUrl}/${id}/delete`;
-    return this.http.delete<Product>(url, {responseType: 'text' as 'json'});
+    return this.http
+      .delete<Product>(url, { responseType: 'text' as 'json' })
+      .pipe(
+        map((object) => object),
+        catchError((error) => this.handleError(error))
+      );
   }
 
+  handleError(error: any): Observable<any> {
+    console.log(error)
+    this.showMessage('Ocorreu algum erro inesperado!', true);
+    return EMPTY;
+  }
 }
