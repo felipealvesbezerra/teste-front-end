e76220218295be163ef1f975c3690d35b3c70ba4
Gabriel Crespo <gabriel.crespo1942@gmail.com>
2020-11-01 01:19:38 -0300

    Tabela de produtos

--- teste-frontend/src/app/components/product/product-read/product-read.component.ts
+++ teste-frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,22 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
 import { Product } from '../product.model';
 import { ProductService } from '../product.service';
+import { ProductReadDataSource } from './product-read-datasource';
 
 @Component({
   selector: 'app-product-read',
   templateUrl: './product-read.component.html',
   styleUrls: ['./product-read.component.css']
 })
-export class ProductReadComponent implements OnInit {
+export class ProductReadComponent implements AfterViewInit, OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatTable) table: MatTable<Product>;
+  dataSource: ProductReadDataSource;
 
-  products: Product[]
+  displayedColumns = ['nome', 'marca', 'modelo', 'preco'];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {
 
-  ngOnInit(): void {
-    this.productService.read().subscribe(products => {
-      this.products = products;
-    })
   }
 
+  ngOnInit() {
+    this.dataSource = new ProductReadDataSource();
+    this.productService.read().subscribe(product => {
+      this.dataSource.data = product
+      })
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
 }
